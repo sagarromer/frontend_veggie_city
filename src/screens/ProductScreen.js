919c57377d8820edc4b4ecb4beed24cec3d762e0
sagara dissanayake <39671491+sagarromer@users.noreply.github.com>
2021-01-24 09:31:28 -0800

    add redux to product screen

--- src/screens/ProductScreen.js
+++ src/screens/ProductScreen.js
@@ -1,35 +1,53 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
 import { Link } from 'react-router-dom';
+import { detailsProduct } from '../actions/productActions';
+import LoadingBox from '../components/LoadingBox';
+import MessageBox from '../components/MessageBox';
 import Rating from '../components/Rating';
-import data from '../data';
+
 
 export default function ProductScreen(props) {
-    const product = data.products.find((x) => x._id === props.match.params.id);
-    if (!product) {
-        return <div> Product Not Found</div>;
-    }
+    const dispatch = useDispatch();
+    const productId = props.match.params.id;
+    const productDetails = useSelector((state) => state.productDetails);
+    const { loading, error, product } = productDetails;
+
+    useEffect(() => {
+        dispatch(detailsProduct(productId));
+    }, [dispatch, productId]);
     return (
+        <div>
+            {loading ? (
+        <LoadingBox></LoadingBox>
+        ) : error ? (
+        <MessageBox variant="danger">{error}</MessageBox>
+        ) : (
         <div>
             <Link to="/">Back to result</Link>
             <div className="row top">
                 <div className="col-2">
-                    <img className="large" src={product.image} alt={product.name}></img>
+                    <img
+                        className="large"
+                        src={product.image}
+                        alt={product.name}
+                    ></img>
                 </div>
                 <div className="col-1">
                     <ul>
                         <li>
-                            <h1>{product.name}</h1>
+                        <h1>{product.name}</h1>
                         </li>
                         <li>
-                            <Rating
-                                rating={product.rating}
-                                numReviews={product.numReviews}
-                            ></Rating>
+                        <Rating
+                            rating={product.rating}
+                            numReviews={product.numReviews}
+                        ></Rating>
                         </li>
                         <li>Pirce : ${product.price}</li>
                         <li>
-                            Description:
-                            <p>{product.description}</p>
+                        Description:
+                        <p>{product.description}</p>
                         </li>
                     </ul>
                 </div>
@@ -38,19 +56,20 @@ export default function ProductScreen(props) {
                         <ul>
                         <li>
                             <div className="row">
-                                <div>Price</div>
-                                <div className="price">${product.price}</div>
+                            <div>Price</div>
+                            <div className="price">${product.price}</div>
                             </div>
                         </li>
                         <li>
                             <div className="row">
-                                <div>Status</div>
-                                <div>
-                                    {product.countInStock > 0 ? (
-                                    <span className="success">In Stock</span>
-                                    ) : (
-                                    <span className="danger">Unavailable</span>                                    )}
-                                </div>
+                            <div>Status</div>
+                            <div>
+                                {product.countInStock > 0 ? (
+                                <span className="success">In Stock</span>
+                                ) : (
+                                <span className="danger">Unavailable</span>
+                                )}
+                            </div>
                             </div>
                         </li>
                         <li>
@@ -60,6 +79,8 @@ export default function ProductScreen(props) {
                     </div>
                 </div>
             </div>
+        </div>
+        )}
         </div>
     );
 }
