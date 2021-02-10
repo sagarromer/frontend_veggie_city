5f43584b0f036fb75d29fc39dd571fa999ee8464
sagara dissanayake <39671491+sagarromer@users.noreply.github.com>
2021-02-10 11:10:31 -0800

    sign in implemented

--- src/store.js
+++ src/store.js
@@ -5,7 +5,10 @@ import {
     productDetailsReducer,
     productListReducer,
 } from './reducers/productReducers';
-import { userSigninReducer } from './reducers/userReducers';
+import {
+    userRegisterReducer,
+    userSigninReducer,
+} from './reducers/userReducers';
 
 const initialState = {
     userSignin: {
@@ -24,6 +27,8 @@ const reducer = combineReducers({
     productDetails: productDetailsReducer,
     cart: cartReducer,
     userSignin: userSigninReducer,
+    userRegister: userRegisterReducer,
+
 
 
 });
