3127858a917d8b8a6647b9d4bfbb44cb384e41d2
sagara dissanayake <39671491+sagarromer@users.noreply.github.com>
2021-02-28 08:20:16 -0800

    adding paypal button and implemented payment

--- src/reducers/orderReducers.js
+++ src/reducers/orderReducers.js
@@ -6,6 +6,10 @@ import {
     ORDER_DETAILS_FAIL,
     ORDER_DETAILS_REQUEST,
     ORDER_DETAILS_SUCCESS,
+    ORDER_PAY_FAIL,
+    ORDER_PAY_REQUEST,
+    ORDER_PAY_RESET,
+    ORDER_PAY_SUCCESS,
 } from '../constants/orderConstants';
 
 export const orderCreateReducer = (state = {}, action) => {
@@ -22,10 +26,8 @@ export const orderCreateReducer = (state = {}, action) => {
             return state;
 }
 };
-export const orderDetailsReducer = (
-    state = { loading: true, order: {} },
-    action
-    ) => {
+export const orderDetailsReducer = (state = { loading: true }, action) => {
+
         switch (action.type) {
         case ORDER_DETAILS_REQUEST:
             return { loading: true };
@@ -36,4 +38,18 @@ export const orderDetailsReducer = (
         default:
             return state;
     }
+};
+export const orderPayReducer = (state = {}, action) => {
+    switch (action.type) {
+        case ORDER_PAY_REQUEST:
+            return { loading: true };
+        case ORDER_PAY_SUCCESS:
+            return { loading: false, success: true };
+        case ORDER_PAY_FAIL:
+            return { loading: false, error: action.payload };
+        case ORDER_PAY_RESET:
+            return {};
+        default:
+            return state;
+        }
 };
